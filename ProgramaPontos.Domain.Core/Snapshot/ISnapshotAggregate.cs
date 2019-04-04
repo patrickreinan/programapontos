54196776ee5b1480e9d30d2905c6e4c359efcdbb
Patrick Reinan <patrickreinan@gmail.com>
2019-04-03 23:00:56 -0300

    Snapshot em desenvolvimento

---
+++ ProgramaPontos.Domain.Core/Snapshot/ISnapshotAggregate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramaPontos.Domain.Core.Snapshot
+{
+    public interface ISnapshotAggregate
+    {
+        
+    }
+}
